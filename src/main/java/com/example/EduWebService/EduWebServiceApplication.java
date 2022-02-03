c7796e18d84c04c5e9d8fcc0307fadca8c7d1d69
elamanzhak <elamanzhak@gmail.com>
2022-02-03 12:13:59 +0600

    asd

---
+++ src/main/java/com/example/EduWebService/EduWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EduWebService/EduWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
